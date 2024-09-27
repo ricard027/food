44f8cd5809609183dcfc96c8d2d757d851a1ff17
Ricardo <ricardo_cabista@hotmail.com>
2024-09-26 21:26:50 -0300

    chore: initial configs added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        secondary: 'var(--background)',
+        accent: 'var(--accent)',
+        primary: 'var(--primary)',
+        textLight: 'var(--text-light)'
       },
-    },
+      screens: {
+        xs: '320px'
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
