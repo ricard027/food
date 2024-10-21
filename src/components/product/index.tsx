70446c0564908a27ad06b70cf1fad5ad8e0bd164
Ricardo <ricardo_cabista@hotmail.com>
2024-10-21 20:47:48 -0300

    feat: initial changes in product

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -1,14 +1,21 @@
 'use client'
-import { MenuItem } from '@/service/getMenuItemService'
+// import { MenuItem } from '@/service/getMenuItemService'
 import Image from 'next/image'
 import { FC, useState } from 'react'
 
-import { GoHeart, GoHeartFill, GoStar } from 'react-icons/go'
+import { GoHeart, GoHeartFill } from 'react-icons/go'
+import { PiBowlFoodThin } from 'react-icons/pi'
 
-const Product: FC<MenuItem> = ({ image, title, price, likes }) => {
+interface IProduct {
+  idMeal: string
+  strMeal: string
+  strMealThumb: string
+}
+
+const Product: FC<IProduct> = ({ idMeal, strMeal, strMealThumb }) => {
   const [favoriteProduct, setFavoriteProduct] = useState(false)
   return (
-    <div className='flex flex-col justify-between bg-[#f1f1f1] gap-6 shadow-lg  rounded-lg flex-1 items-center relative '>
+    <div className='flex flex-col justify-between bg-[#f1f1f1] gap-6 shadow-lg  rounded-lg  items-center relative mb-4'>
       <div
         className='absolute flex items-center justify-center  left-4 top-4 cursor-pointer z-10'
         onClick={() => setFavoriteProduct((prev) => !prev)}
@@ -19,23 +26,22 @@ const Product: FC<MenuItem> = ({ image, title, price, likes }) => {
         <Image
           width={150}
           height={150}
-          alt={title}
-          src={image}
-          className='mix-blend-multiply max-h-40 '
+          alt={strMeal}
+          src={strMealThumb}
+          className='mix-blend-multiply max-h-40 rounded-full '
         />
       </div>
       <div className='flex flex-col gap-6 p-4 w-full items-center bg-white rounded-b-lg rounded-t-3xl'>
-        <div>
-          <p className='line-clamp-1' title={title}>
-            {title}
+        <div className='w-full'>
+          <p className='flex items-center gap-4 w-full ' title={strMeal}>
+            <PiBowlFoodThin size={20} className='text-accent' />
+            <span className='line-clamp-1 text-gray-600 max-w-[80%]'>
+              {strMeal}
+            </span>
           </p>
         </div>
         <div className='flex justify-between w-full'>
-          <p>R${price}</p>
-          <div className='flex items-center justify-center '>
-            {likes}
-            <GoStar />
-          </div>
+          <p>R${idMeal}</p>
         </div>
       </div>
     </div>
