dce3f5adb63108e94d31926d633a85cd5ebc0d6c
Ricardo <ricardo_cabista@hotmail.com>
2024-11-26 21:18:24 -0300

    feat: Some changes in button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,13 +1,13 @@
-import { FC, ReactNode } from "react";
+import { FC, HTMLAttributes, ReactNode } from "react";
 
-interface IButton {
+interface IButton extends HTMLAttributes<HTMLButtonElement> {
   icon?: ReactNode;
   children: ReactNode;
 }
 
-const Button: FC<IButton> = ({ children, icon }) => {
+const Button: FC<IButton> = ({ children, icon ,...props}) => {
   return (
-    <button className="bg-primary flex items-center gap-4 rounded-3xl px-4 py-2 font-semibold">
+    <button className="bg-primary flex items-center gap-4 rounded-3xl px-4 py-2 font-semibold" {...props}>
       {icon}
       {children}
     </button>
