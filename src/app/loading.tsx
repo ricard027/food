e5f2179da3b475872d3017debeb831ff4e505288
Ricardo <ricardo_cabista@hotmail.com>
2025-01-14 20:07:05 -0300

    feat: Initial loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,13 @@
+import { CgSpinner } from 'react-icons/cg'
+
 const Loding = () => {
-  return <div>carregando</div>
+  return (
+    <div className='h-screen bg-gray-50 backdrop-blur-2xl flex items-center justify-center z-50 w-full'>
+      <div className='flex flex-col items-center gap-2'>
+        <p className='text-lg'>carregando...</p>
+        <CgSpinner size={30} className='animate-spin' />
+      </div>
+    </div>
+  )
 }
 export default Loding
