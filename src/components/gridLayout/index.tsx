9f698683601118c5c0acfadf3fa22317e7a84377
Ricardo <ricardo_cabista@hotmail.com>
2025-01-18 11:12:21 -0300

    feat: changes in grid component layout

--- src/components/gridLayout/index.tsx
+++ src/components/gridLayout/index.tsx
@@ -1,8 +1,6 @@
-import { FC, PropsWithChildren } from 'react'
+import { FC, ReactNode } from 'react'
 
-interface IGridLayout extends PropsWithChildren {}
-
-const GridLayout: FC<IGridLayout> = ({ children }) => {
+const GridLayout: FC<{children:ReactNode}> = ({ children }) => {
   return (
     <section className='max-w-6xl grid sm:grid-cols-3  xs:grid-cols-1 flex-col gap-4 lg:grid-cols-4  md:grid-cols-3 m-auto'>
       {children}
