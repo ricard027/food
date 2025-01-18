15bc7e2c4792bb00b6bb0e9f016177411a0f5a80
Ricardo <ricardo_cabista@hotmail.com>
2025-01-18 11:11:03 -0300

    feat: grid component created

---
+++ src/components/gridLayout/index.tsx
@@ -0,0 +1,13 @@
+import { FC, PropsWithChildren } from 'react'
+
+interface IGridLayout extends PropsWithChildren {}
+
+const GridLayout: FC<IGridLayout> = ({ children }) => {
+  return (
+    <section className='max-w-6xl grid sm:grid-cols-3  xs:grid-cols-1 flex-col gap-4 lg:grid-cols-4  md:grid-cols-3 m-auto'>
+      {children}
+    </section>
+  )
+}
+
+export default GridLayout
