ca0433a4bff81f1d3b23b93ae1ff0e0621e70fb9
Ricardo <ricardo_cabista@hotmail.com>
2025-01-20 19:00:04 -0300

    fix: testing new changes

--- src/app/menu/[id]/page.tsx
+++ src/app/menu/[id]/page.tsx
@@ -3,35 +3,32 @@ import { filters } from './options-filter-category.json'
 
 import MenuView from './menuView'
 import ProductDetails from './recipe/page'
-import IF from '@/components/IF'
-import { useRouter } from 'next/router'
 
-const MenuPage = async ({ params }: any) => {
-  
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const router = useRouter()
-  const { query } = router
+export const dynamic = 'force-dynamic'
 
+const MenuPage = async ({ params, ...props }: any) => {
   const categoryByParams = params?.id
-  const _searchParams = query?.id
-  const containSearch = typeof _searchParams === 'string'
+  const _searchParams = props?.searchParams?.id
 
-  if (containSearch) {
+  if (typeof _searchParams === 'string') {
     return <ProductDetails searchParams={_searchParams} />
   }
 
-  const { meals: products } = await GetProductsByCategoryService({
-    category: categoryByParams
-  })
+  const { meals: products = [] } =
+    (await GetProductsByCategoryService({
+      category: categoryByParams
+    })) 
+
 
   return (
-    <IF condition={!containSearch}>
+    <>
       <MenuView
         categoryByParams={categoryByParams}
         filters={filters}
         products={products}
       />
-    </IF>
+    </>
   )
 }
+
 export default MenuPage
