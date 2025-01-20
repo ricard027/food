80d5e4c93f93fce3875e47bbb9de434af3641d1a
ricardo machado <ricardo_cabista@hotmail.com>
2025-01-20 15:37:03 -0300

    fix: change  testing to redeploy

--- src/app/menu/[id]/page.tsx
+++ src/app/menu/[id]/page.tsx
@@ -4,10 +4,16 @@ import { filters } from './options-filter-category.json'
 import MenuView from './menuView'
 import ProductDetails from './recipe/page'
 import IF from '@/components/IF'
+import { useRouter } from 'next/router'
 
-const MenuPage = async ({ params, ...props }: any) => {
-  const categoryByParams = await params?.id
-  const _searchParams = await props?.searchParams?.id
+const MenuPage = async ({ params }: any) => {
+  
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter()
+  const { query } = router
+
+  const categoryByParams = params?.id
+  const _searchParams = query?.id
   const containSearch = typeof _searchParams === 'string'
 
   if (containSearch) {
@@ -17,8 +23,6 @@ const MenuPage = async ({ params, ...props }: any) => {
   const { meals: products } = await GetProductsByCategoryService({
     category: categoryByParams
   })
-
-
 
   return (
     <IF condition={!containSearch}>
