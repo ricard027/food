c340362a134b09220adee7eeebdadf31d9d3726e
Ricardo <ricardo_cabista@hotmail.com>
2025-02-02 13:38:47 -0300

    feat: product details page removed of menu page

--- src/app/menu/[id]/page.tsx
+++ src/app/menu/[id]/page.tsx
@@ -5,16 +5,13 @@ import { usePathname, useSearchParams } from 'next/navigation'
 import { MenuModel } from './menuModel'
 
 import MenuView from './menuView'
-import ProductDetails from './recipe/page'
 
 const MenuPage = () => {
   const pathname = usePathname()
   const searchParams = useSearchParams()
   const _searchParams = searchParams.get('id')
 
-  if (_searchParams) { 
-    return <ProductDetails searchParams={_searchParams} />
-  }
+
 
   const  products  = MenuModel(pathname.split('/')[2])
 
